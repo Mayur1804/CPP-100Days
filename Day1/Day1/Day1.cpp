088bd29256184a3dc81a924fdb1416085f3e41b5
Mayur1804 <55289169+Mayur1804@users.noreply.github.com>
2024-04-10 15:08:22 +0530

    recommit test

---
+++ Day1/Day1/Day1.cpp
@@ -0,0 +1,16 @@
+
+#include "testTwo.h"
+#include <iostream>
+
+int main()
+{
+    
+    testTwo aa;
+    aa.printMessage();
+    int a;
+    std::cout << "Enter Int";
+    std::cin >> a;
+    std::cout << "Hello World!\n";
+    std::cout << a;
+}
+
---
+++ Day1/Day1/testTwo.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+class testTwo
+{
+public:
+    void printMessage()
+    {
+        std::cout << "HelloClass";
+    }
+};
+
