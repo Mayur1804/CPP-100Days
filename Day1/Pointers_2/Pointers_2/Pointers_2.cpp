061f0021afe1a02b8a2728a1daea7698f167e1ba
Mayur1804 <55289169+Mayur1804@users.noreply.github.com>
2024-04-11 16:21:39 +0530

    explanation about Void Pointers

---
+++ Day1/Pointers_2/Pointers_2/Pointers_2.cpp
@@ -0,0 +1,23 @@
+// Pointers_2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Void Pointer - Can point to any datatype
+
+#include <iostream>
+using namespace std;
+
+void print(void* ptr, char type) {                       // Defining a function named print that takes a void pointer and a character type as arguments.
+	switch (type){                                       // Switching based on the character type provided.
+	case 'i':cout << *((int*)ptr) << endl; break;        // Casting the void pointer to an integer pointer and dereferencing it to print the integer value.
+	case 'c':cout << *((char*)ptr) << endl; break;	     // Casting the void pointer to a character pointer and dereferencing it to print the character value.
+	} 
+}
+
+int main()
+{
+	int number = 12;
+	char letter = 'd';
+	
+	print(&letter, 'c');
+	print(&number, 'i');
+}
+
+
